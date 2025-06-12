021dac8daeea0451153b6a70d0a4aefb8972dea6
Alexander Vishnevskiy <alexander.vishnevskiy@cerebras.net>
2025-06-12 10:20:11 -0700

    Add default paths

--- analyze.py
+++ analyze.py
@@ -1,21 +1,25 @@
+import json
+import os
+from argparse import ArgumentParser
 from json import loads
 from typing import Dict, List
-from argparse import ArgumentParser
-from dacite import from_dict, Config
+
+from dacite import Config, from_dict
 
 from core.evaluator import evaluate
 from core.types import GenerationOutput
-from core.utils import print_scores, plot_perf_metrics, save_scores
-import json
-import os
+from core.utils import plot_perf_metrics, print_scores, save_scores
+
+OUTPUT_PATH = "outputs/json_schema_bench"
+FAILED_SCHEMAS_OUTPUT_PATH = "outputs/failed_schemas"
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--outputs", type=str, required=True)
     parser.add_argument("--details", action="store_true")
-    parser.add_argument("--output-path", type=str, required=False, default=None)
-    parser.add_argument("--failed-schemas-output-path", type=str, required=False, default=None)
+    parser.add_argument("--output-results", action="store_true")
+    parser.add_argument("--output-failed-schemas", action="store_true")
     args = parser.parse_args()
 
     dacite_config = Config(check_types=False)
@@ -39,30 +43,30 @@ if __name__ == "__main__":
     empirical_coverage = []
     for task_name, outputs in task_outputs.items():
         dc, ec, cl, pm, ot, categories_cov, failed_schemas = evaluate(outputs)
-        
-        if args.output_path:
-            os.makedirs(args.output_path, exist_ok=True)
-            path = os.path.join(args.output_path, f"{task_name}_category_coverage.json")
+
+        if args.output_results:
+            os.makedirs(OUTPUT_PATH, exist_ok=True)
+            path = os.path.join(OUTPUT_PATH, f"{task_name}_category_coverage.json")
             with open(path, "w") as f:
                 json.dump(categories_cov, f)
-                
-        if args.failed_schemas_output_path:
-            os.makedirs(args.failed_schemas_output_path, exist_ok=True)
+
+        if args.output_failed_schemas:
+            os.makedirs(FAILED_SCHEMAS_OUTPUT_PATH, exist_ok=True)
             for cat in categories_cov.keys():
-                path = os.path.join(args.failed_schemas_output_path, cat)
+                path = os.path.join(FAILED_SCHEMAS_OUTPUT_PATH, cat)
                 os.makedirs(path, exist_ok=True)
-                
+
                 path = os.path.join(path, f"{task_name}_failed_schemas.json")
                 with open(path, "w") as f:
                     json.dump(failed_schemas, f)
-                    
+
         compliance.append(cl)
         perf_metrics.append(pm)
         declared_coverage.append(dc)
         empirical_coverage.append(ec)
         output_tokens.append(ot)
 
-    if args.output_path:
+    if args.output_results:
         save_scores(
             declared_coverage,
             empirical_coverage,
@@ -70,9 +74,9 @@ if __name__ == "__main__":
             perf_metrics,
             output_tokens,
             list(task_outputs.keys()),
-            args.output_path,
+            OUTPUT_PATH,
         )
-    
+
     print(engine_config)
     print_scores(
         declared_coverage,
